32d88cb5a268155dd23611b96e329cae276a70a4
FAIZA IBRAHIM SURAW <itssumeya@gmail.com>
2025-07-20 14:09:31 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,29 +1,14 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
 from .models import Book
 from .models import Library
-
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.select_related('author').all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view to show library detail
+#  Class-based view to show details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-
-# User registration view
-def register_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  # Automatically log in after registering
-            return redirect('list_books')
-    else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
