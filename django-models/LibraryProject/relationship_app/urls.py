7e205addc029078aea2db58d991840c5fd36fd98
FaizaSuraw <itssumeya@gmail.com>
2025-07-20 14:00:13 +0300

    fix:urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
+from . import views
 from django.contrib.auth import views as auth_views
-from .views import register_view, list_books, LibraryDetailView
 
 urlpatterns = [
-    path('register/', register_view, name='register'),
+    # Book and library views
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # Authentication views
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-
-    # Existing views
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', views.register, name='register'),
 ]
