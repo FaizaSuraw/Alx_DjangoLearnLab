2fd26d1392ad14990315fd407efc0af6f65b37df
FAIZA IBRAHIM SURAW <itssumeya@gmail.com>
2025-07-20 14:59:13 +0300

    added permissions urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,6 +6,9 @@ from .views import Logout
 from .views import admin_view
 from .views import librarian_view
 from .views import member_view
+from .views import add_book
+from .views import edit_book
+from .views import delete_book
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
@@ -15,9 +18,15 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout
 
-# Role-based access URLs
-path('admin-role/', admin_view, name='admin_view'),
-path('librarian-role/', librarian_view, name='librarian_view'),
-path('member-role/', member_view, name='member_view'),
+    # Task 3: Role-based access URLs
+    path('admin-role/', admin_view, name='admin_view'),
+    path('librarian-role/', librarian_view, name='librarian_view'),
+    path('member-role/', member_view, name='member_view'),
+
+    # Task 4 - Book management with custom permissions
+    path('books/add/', add_book, name='add_book'),
+    path('books/edit/<int:book_id>/', edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', delete_book, name='delete_book'),
+
 
 ]
