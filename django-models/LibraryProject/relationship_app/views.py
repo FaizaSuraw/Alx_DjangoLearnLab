7846b19e743369e4d75d335bc27f034548914f10
FaizaSuraw <itssumeya@gmail.com>
2025-07-20 13:55:04 +0300

    added authentication routes, register and login

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic.detail import DetailView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import Book
 from .models import Library
 
-
 # Function-based view to list all books
 def list_books(request):
-    books = Book.objects.all()
+    books = Book.objects.select_related('author').all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view for a specific library
+# Class-based view to show library detail
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# User registration view
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log in after registering
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
