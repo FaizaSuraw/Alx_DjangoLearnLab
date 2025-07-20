e8b1aa6e1961ea678b1ea0c3e351321abf9dfaf0
FAIZA IBRAHIM SURAW <itssumeya@gmail.com>
2025-07-20 14:38:21 +0300

    added role-based access urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,5 +13,11 @@ urlpatterns = [
     # Task 2: Auth Views
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout
+
+# Role-based access URLs
+path('admin-role/', admin_view, name='admin_view'),
+path('librarian-role/', librarian_view, name='librarian_view'),
+path('member-role/', member_view, name='member_view'),
+
 ]
