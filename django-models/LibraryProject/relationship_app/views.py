8d6b3b78d1a935ba5b4c1a504871aa3b0bc751d9
FAIZA IBRAHIM SURAW <itssumeya@gmail.com>
2025-07-20 14:25:29 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login
 
 # ✅ Function-based view to list all books
 def list_books(request):
@@ -16,14 +16,22 @@ class LibraryDetailView(DetailView):  # ← checker wants to see DetailView used
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-# User registration view
-def register_view(request):
+# Task 2 - Registration view (uses login() after registration)
+def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Automatically log in after registering
+            login(request, user)  # Log the user in after registration
             return redirect('list_books')
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+# Task 2 - Login view
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+# Task 2 - Logout view
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
