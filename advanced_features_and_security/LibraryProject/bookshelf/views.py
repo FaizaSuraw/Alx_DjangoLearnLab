6182646ca89b5ccdd32069fc1a65507e4514d11b
FaizaSuraw <itssumeya@gmail.com>
2025-07-27 15:41:50 +0300

    fixes

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponse
-
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Book
 
 def home_view(request):
     return HttpResponse("📚 Welcome to the Bookshelf App")
@@ -13,3 +13,8 @@ def view_books(request):
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
     return HttpResponse("You have permission to create a book.")
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
