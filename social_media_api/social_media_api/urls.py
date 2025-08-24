b8f84f9e95de01a323f329d8ff2f57f885bb4994
FaizaSuraw <itssumeya@gmail.com>
2025-08-24 11:59:08 +0300

    added a home route

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("API is live!")
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', home), 
     path("accounts/", include("accounts.urls")),
     path("api/", include("posts.urls")),
 ]
